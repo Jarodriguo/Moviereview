6a923e41f2266cb6954c4bd978e90e6728310415
Jarodriguo <jarodriguo@eafit.edu.co>
2023-08-23 11:55:46 -0500

    Avances taller 2

--- movie/views.py
+++ movie/views.py
@@ -9,11 +9,17 @@ def home(request):
     # return HttpResponse('<h1>Welcome to Home Page </h1>')
     # return render(request, 'home.html')
     # return render(request, 'home.html', {'name': 'Juan Alberto'})
+    # movies = Movie.objects.all()
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
 
 
 
